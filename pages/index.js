30efd9345458c282f01f4c268822b3e716f0f44a
eidas <eidagm@gmail.com>
2025-08-24 01:13:57 -0700

    .envの修正

--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,12 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_FIREBASE_API_KEY || "your-api-key",
   authDomain:
     process.env.NEXT_FIREBASE_AUTH_DOMAIN || "your-project.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-project-id",
+  projectId: process.env.NEXT_FIREBASE_PROJECT_ID || "your-project-id",
   storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-    "your-project.appspot.com",
+    process.env.NEXT_FIREBASE_STORAGE_BUCKET || "your-project.appspot.com",
   messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "123456789",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-app-id",
+    process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID || "123456789",
+  appId: process.env.NEXT_FIREBASE_APP_ID || "your-app-id",
 };
 
 const app = initializeApp(firebaseConfig);
